c88979edc3547831ec45ef078669fb610e999417
Marcel <marcel.dumitru@live.at>
2025-12-08 03:18:36 +0100

    replace formfields with angular materials form fields

--- src/app/dashboard/add-data/add-data.ts
+++ src/app/dashboard/add-data/add-data.ts
@@ -2,10 +2,26 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '../../shared/store';
 import { Backend } from '../../shared/backend';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule }      from '@angular/material/input';
+import { MatSelect, MatOption }     from '@angular/material/select';
+import { MatCheckbox }   from '@angular/material/checkbox';
+import { MatDatepickerModule, MatDatepickerInput, MatDatepickerToggle, MatDatepicker } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAnchor }     from '@angular/material/button';
 
 @Component({
   selector: 'app-add-data',
-  imports: [ReactiveFormsModule],
+  standalone: true,
+  imports: [ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInputModule,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDatepickerModule,
+    MatNativeDateModule, MatSelect, MatOption, MatCheckbox, MatAnchor],
   templateUrl: './add-data.html',
   styleUrl: './add-data.scss',
 })
@@ -20,7 +36,19 @@ export class AddData {
       name: ['', Validators.required],
       birthdate: ['', Validators.required],
       courseId: ['', Validators.required],
+      newsletter: [false],
+      email: [{ value: '', disabled: true}, [Validators.email]]
     });
+
+    this.signupForm.get('newsletter')?.valueChanges.subscribe((isChecked: boolean) => {
+      const emailControl = this.signupForm.get('email');
+      if (isChecked) {
+        emailControl?.enable();
+      } else {
+        emailControl?.disable();
+        emailControl?.reset();
+      }
+    })
   }
 
   onSubmit() {
